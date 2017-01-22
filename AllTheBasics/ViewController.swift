07b6c5cd0459bd9618beb1b89459f37fe4b3f30b
Oliver-Moffat <oliver.locke.moffat@gmail.com>
2017-01-22 11:47:04 -0500

    Edited Test

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -12,9 +12,52 @@ class ViewController: UIViewController {
     
     
     
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        
+        
+        if ((a + b + c) / 3.0) > 75 {
+            return true
+        } else {
+            return false
+        }
+        
+        
+    }
+    
+    func passwordCombo(username: String, password: Int) -> String {
+        
+        if username == "Jerry" && password % 3 == 0 {
+            return "Welcome!"
+        } else if username == "Elaine" && password % 3 == 0 {
+            return "Welcome!"
+        } else if username == "Michael" && password % 3 == 0 {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
     
     
-    // Implement your functions here
+  
+    
+    func describe(emoji: String) -> String {
+        
+        if emoji == "💋" {
+            return "Kiss"
+        } else if emoji == "🐈" {
+            return "Cat"
+        } else if emoji == "🐢" {
+            return "Turtle"
+        } else if emoji == "🍕" {
+            return "Pizza"
+        } else if emoji == "👻" {
+            return "Ghost"
+        } else {
+            return "Unknown"
+        }
+        
+    }
+
     
     
     
